9650133a8229388611e929e5f929cb690f90c752
CopperWar <copper5949yt@gmail.com>
2024-09-03 21:41:50 +0530

    Senso

---
+++ Qnas/missing.java
@@ -0,0 +1,19 @@
+
+public class missing {
+
+    public static void main(String[] args) {
+
+        int array[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 10};
+        int xor = 0;
+
+        for (int i = 0; i <= 10; i++) {
+            xor = xor ^ i;
+        }
+
+        for (int num : array) {
+            xor = xor ^ num;
+        }
+
+        System.out.println(xor);
+    }
+}
